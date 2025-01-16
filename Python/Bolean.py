a5582cabf34712481d353e409af0c9410df8d6ce
Ravenn <ikmalmhmmd09@gmail.com>
2025-01-16 16:14:33 +0700

    Bolean.py

--- Python/Bolean.py
+++ Python/Bolean.py
@@ -1,10 +1,6 @@
-# THE KEYWORD JUST CHANGE A SUBJECT
-from ctypes.wintypes import POINT
-
-
+# THE KEYWORD JUST CHANGE A SUBJECT TO BE TRUE OR FALSE
 umur = 8
 if (umur == 10) :
     print("FAKE")
 else :
     print("REAL\n")
-
